8803ec6fb1c1d87d2dfa947b3efd86c7096afaa8
Antonio Guzman <aguzmanv09@gmail.com>
2020-07-06 13:58:31 -0500

    Arreglando bugs del video player

--- src/frontend/react/pages/VideoPlayer.jsx
+++ src/frontend/react/pages/VideoPlayer.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
 import VideoCard from '../components/cards/VideoCard';
 import PdfPreview from '../components/cards/PdfPreview';
-import Video1 from '../../assets/images/video1.jpg';
-import Video2 from '../../assets/images/video2.jpg';
-import Video3 from '../../assets/images/video5.jpg';
-import Video4 from '../../assets/images/video6.jpg';
+import Video from '../../assets/images/draw-video.png';
+import NotFound from './NotFound';
 
 const VideoPlayer = (props) => {
   const { institute, location } = props;
   const str = location.pathname.replace(/\/player\//g, '');
   const [video] = institute.videos.filter((video) => video._id === str);
   // console.log(institute.videos);
+  const [error, setError] = useState(video?.url == null);
+
+  if (error) {
+    return <NotFound />;
+  }
+
   return (
     <section className='VideoPlayer'>
       <ReactPlayer
         className='VideoPlayer--player'
         controls
-        url={video?.url || 'https://youtu.be/uWdncyCvSwQ'}
+        url={video?.url}
         width='60vw'
         height='70vh'
         // onReady={() => { console.log('onReady callback'); }}
         // onStart={() => { console.log('onStart callback'); }}
         // onPause={() => { console.log('onPaused callback'); }}
         // onEnded={() => { console.log('onEnded callback'); }}
-        // onError={() => { console.log('onError callback'); }}
+        onError={() => setError(true)}
       />
       <div className='VideoPlayer__preview'>
-        <VideoCard videoPreview={Video1} />
+        <VideoCard videoPreview={Video} />
         <div className='border-thin border-top' />
-        <VideoCard videoPreview={Video2} />
+        <VideoCard videoPreview={Video} />
         <div className='border-thin border-top' />
-        <VideoCard videoPreview={Video3} />
+        <VideoCard videoPreview={Video} />
         <div className='border-thin border-top' />
-        <VideoCard videoPreview={Video4} />
+        <VideoCard videoPreview={Video} />
         <div className='border-thin border-top' />
       </div>
       <div className='VideoPlayer__info'>
@@ -59,10 +63,11 @@ const VideoPlayer = (props) => {
           <p>{video?.description || 'Video description'}</p>
         </div>
 
-        <PdfPreview />
+        <PdfPreview name={video?.name} />
       </div>
     </section>
   );
+
 };
 
 const mapStateToProps = (state) => {
